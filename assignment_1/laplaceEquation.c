767b905f8ed9ce70440cbb035d2d55746b29351b
anavp <anavprasadanav@gmail.com>
2022-02-13 18:49:41 -0500

    jacobi and gauss seidel methods correctly implemented

--- assignment_1/laplaceEquation.c
+++ assignment_1/laplaceEquation.c
@@ -3,7 +3,8 @@
 #include <assert.h>
 #include <math.h>
 #include <stdbool.h>
-#define UPPER_LIMIT_N 1000
+#include<unistd.h>
+#define UPPER_LIMIT_N 10005
 #define ITER_LIMIT 5000
 #define MAX_RES_DECREASE 1000000
 
@@ -16,7 +17,7 @@ int getNum(char ch){
 }
 
 
-void printVector(double vec[], int n){
+void printVector(double *vec, int n){
     for (int i = 0; i < n; ++i)
         printf("%lf ", vec[i]);
     printf("\n");
@@ -31,64 +32,53 @@ int readInt(char ar[]){
 }
 
 
-void init(double A[][UPPER_LIMIT_N], double f[], double u[], int n){
+int elementAccess(int i, int j, int n){
+    return (i * n + j);
+}
+
+
+void init(double *A, double *f, double *u, int n){
     for (int i = 0; i < n; ++i){
         f[i] = 1;
-        // TODO: 0 init?
         u[i] = 0;
         for (int j = 0; j < n; ++j){
-            if (i == j) A[i][j] = 2;
-            else if (abs(i - j) == 1) A[i][j] = -1;
-            else A[i][j] = 0;
+            if (i == j) A[elementAccess(i, j, n)] = 2;
+            else if (abs(i - j) == 1) A[elementAccess(i, j, n)] = -1;
+            else A[elementAccess(i, j, n)] = 0;
         }
     }
 }
 
 
-void deepCopyMatrix(double src[][UPPER_LIMIT_N], double dest[][UPPER_LIMIT_N], int n){
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j)
-            dest[i][j] = src[i][j];
-}
-
-
-void deepCopyVector(double src[], double dest[], int n){
+void deepCopyVector(double *src, double *dest, int n){
     for (int i = 0; i < n; ++i)
             dest[i] = src[i];
 }
 
 
-double compute2Norm(double vec[], int n){
+double compute2Norm(double *vec, int n){
     double sum = 0;
     for (int i = 0; i < n; ++i) sum += vec[i]*vec[i];
     return sqrt(sum);
 }
 
 
-void multiply(double matrix[][UPPER_LIMIT_N], double vec[], double ans[], int n){
+void multiply(double *matrix, double *vec, double *ans, int n){
     for (int i = 0; i < n; ++i){
         ans[i] = 0;
-        for (int j = 0; j < n; ++j){
-            ans[i] += matrix[i][j] * vec[j];
-        }
+        for (int j = 0; j < n; ++j)
+            ans[i] += matrix[elementAccess(i, j, n)] * vec[j];
     }
 }
 
 
-// stores the answer in vec1
-void subtract(double vec1[], double vec2[], int n){
+void subtract(double *vec1, double *vec2, int n){
     for (int i = 0; i < n; ++i)
         vec1[i] = vec1[i] - vec2[i];
 }
 
 
-void subtractAndStore(double vec1[], double vec2[], double ans[], int n){
-    for (int i = 0; i < n; ++i)
-        ans[i] = vec1[i] - vec2[i];
-}
-
-
-double computeResidual(double A[][UPPER_LIMIT_N], double f[], double u[], int n){
+double computeResidual(double *A, double *f, double *u, int n){
     double ans[n];
     multiply(A, u, ans, n);
     subtract(ans, f, n);
@@ -98,38 +88,37 @@ double computeResidual(double A[][UPPER_LIMIT_N], double f[], double u[], int n)
 
 bool checkIfDone(double initialResidual, double currentResidual, int iterCount){
     if (iterCount >= ITER_LIMIT) return true;
-    if (initialResidual - currentResidual >= MAX_RES_DECREASE) return true;
-    return false;
+    return (initialResidual - currentResidual >= MAX_RES_DECREASE);
 }
 
 
-void jacobiUpdate(double A[][UPPER_LIMIT_N], double f[], double u[], int n){
-    double prevU[n];
+void jacobiUpdate(double *A, double *f, double *u, int n){
+    double *prevU = malloc(n * sizeof(double));
     deepCopyVector(u, prevU, n);
     for (int i = 0; i < n; ++i){
         u[i] = 0;
         for (int j = 0; j < n; ++j){
             if (j == i) continue;
-            u[i] += (A[i][j] * prevU[j]);
+            u[i] += (A[elementAccess(i, j, n)] * prevU[j]);
         }
-        u[i] = (f[i] - u[i])/A[i][i];
+        u[i] = (f[i] - u[i])/A[elementAccess(i, i, n)];
     }
 }
 
 
-void gaussSeidelUpdate(double A[][UPPER_LIMIT_N], double f[], double u[], int n){
+void gaussSeidelUpdate(double *A, double *f, double *u, int n){
     for (int i = 0; i < n; ++i){
         u[i] = 0;
         for (int j = 0; j < n; ++j){
             if (j == i) continue;
-            u[i] += (A[i][j] * u[j]);
+            u[i] += (A[elementAccess(i, j, n)] * u[j]);
         }
-        u[i] = (f[i] - u[i])/A[i][i];
+        u[i] = (f[i] - u[i])/A[elementAccess(i, i, n)];
     }
 }
 
 
-void update(double A[][UPPER_LIMIT_N], double f[], double u[], int n, method updateMethod){
+void update(double *A, double *f, double *u, int n, method updateMethod){
     switch(updateMethod){
         case JACOBI: jacobiUpdate(A, f, u, n); break;
         case GAUSS_SEIDEL: gaussSeidelUpdate(A, f, u, n); break;
@@ -137,24 +126,38 @@ void update(double A[][UPPER_LIMIT_N], double f[], double u[], int n, method upd
 }
 
 
-void solve(double A[][UPPER_LIMIT_N], double f[], double u[], int n){
+void solve(double *A, double *f, double *u, int n, method updateMethod){
     double initialResidual = computeResidual(A, f, u, n), currentResidual;
     currentResidual = initialResidual;
     int iterCount = 0;
+    printf("initial residual = %lf\n", currentResidual);
     while(!checkIfDone(initialResidual, currentResidual, iterCount)){
         iterCount++;
-        update(A, f, u, n, JACOBI);
+        update(A, f, u, n, updateMethod);
         currentResidual = computeResidual(A, f, u, n);
+        printf("residual = %lf\n", currentResidual);
     }
+}
+
+
+method getUpdateMethod(int val){
+    switch(val){
+        case 0: return JACOBI;
+        case 1: return GAUSS_SEIDEL;
+    }
+    return JACOBI;
 }
 
 
 int main(int argc, char *argv[]){
     assert(argc != 1);
     int n = readInt(argv[1]);
-    double A[n][UPPER_LIMIT_N], f[n], u[n];
+    method updateMethod = getUpdateMethod(readInt(argv[2]));
+    double *A, *f, *u;
+    A = malloc((n*n) * sizeof(double));
+    u = malloc((n) * sizeof(double));
+    f = malloc((n) * sizeof(double));
     init(A, f, u, n);
-    solve(A, f, u, n);
-    printVector(u, n);
+    solve(A, f, u, n, updateMethod);
     return 0;
 }
