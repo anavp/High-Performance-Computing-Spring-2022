a46b9ef66984009088652a8972376bc54d5454d6
anavp <anavprasadanav@gmail.com>
2022-02-14 08:38:58 -0500

    results

--- assignment_1/problem3/laplaceEquationLong.c
+++ assignment_1/problem3/laplaceEquationLong.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <math.h>
 #include <stdbool.h>
 #include <time.h>
+
 #define ITER_LIMIT 100
 #define MAX_RES_DECREASE 1000000.0
 #define USE_INFORMATION_ABOUT_A_STRUCTURE true
+
 typedef enum method{JACOBI, GAUSS_SEIDEL} method;
 
-int getNum(char ch){ return ((int)(ch - '0')); }
+
+int getNum(char ch){
+    return ((int)(ch - '0'));
+}
+
+
+void printVector(double *vec, int n){
+    for (int i = 0; i < n; ++i)
+        printf("%lf ", vec[i]);
+    printf("\n");
+}
+
 
 int readInt(char ar[]){
     int n = 0, i = 0;
@@ -16,7 +30,11 @@ int readInt(char ar[]){
     return n;
 }
 
-int getIndex(int i, int j, int n){ return (i * n + j); }
+
+int getIndex(int i, int j, int n){
+    return (i * n + j);
+}
+
 
 void init(double *A, double *f, double *u, int n){
     for (int i = 0; i < n; ++i){
@@ -30,27 +48,62 @@ void init(double *A, double *f, double *u, int n){
     }
 }
 
+
 void deepCopyVector(double *src, double *dest, int n){
     for (int i = 0; i < n; ++i)
             dest[i] = src[i];
 }
 
-double computeResidual(double *A, double *f, double *u, int n){
-    double norm = 0, temp;
-    for (int i = 0; i < n; ++i){
-        temp = A[getIndex(i, i, n)] * u[i];
-        if (i > 0) temp += A[getIndex(i, i - 1, n)] * u[i-1];
-        if (i < n - 1) temp += A[getIndex(i, i + 1, n)] * u[i+1];
-        temp -= f[i];
-        norm += temp * temp;
-    }
-    return sqrt(norm);
+
+double compute2Norm(double *vec, int n){
+    double sum = 0;
+    for (int i = 0; i < n; ++i) sum += vec[i]*vec[i];
+    return sqrt(sum);
 }
+
+
+void multiply(double *matrix, double *vec, double *ans, int n){
+    for (int i = 0; i < n; ++i){
+        ans[i] = 0;
+        for (int j = 0; j < n; ++j)
+            ans[i] += matrix[getIndex(i, j, n)] * vec[j];
+    }
+}
+
+
+void subtract(double *vec1, double *vec2, int n){
+    for (int i = 0; i < n; ++i)
+        vec1[i] = vec1[i] - vec2[i];
+}
+
+
+double computeResidual(double *A, double *f, double *u, int n){
+    #if USE_INFORMATION_ABOUT_A_STRUCTURE
+        double norm = 0, temp;
+        for (int i = 0; i < n; ++i){
+            temp = A[getIndex(i, i, n)] * u[i];
+            if (i > 0) temp += A[getIndex(i, i - 1, n)] * u[i-1];
+            if (i < n - 1) temp += A[getIndex(i, i + 1, n)] * u[i+1];
+            temp -= f[i];
+            norm += temp * temp;
+        }
+        norm = sqrt(norm);
+    #else
+        double *ans = malloc(n * sizeof(double));
+        multiply(A, u, ans, n);
+        subtract(ans, f, n);
+        double norm = compute2Norm(ans, n);
+        free(ans);
+    #endif
+    return norm;
+}
+
 
 bool checkIfDone(double initialResidual, double currentResidual, int iterCount){
     if (iterCount >= ITER_LIMIT) return true;
     return (currentResidual <= initialResidual/((double)MAX_RES_DECREASE));
 }
+
 
 void update(double *A, double *f, double *u, int n, method updateMethod){
     double *prevU;
@@ -60,17 +113,29 @@ void update(double *A, double *f, double *u, int n, method updateMethod){
     }
     for (int i = 0; i < n; ++i){
         u[i] = 0;
-        if (i > 0){
-            if (updateMethod == JACOBI)
-                u[i] += A[(getIndex(i, i-1, n))] * prevU[i-1];
-            else
-                u[i] += A[(getIndex(i, i-1, n))] * u[i-1];
-        }
-        if (i < n - 1) u[i] += A[getIndex(i, i + 1, n)] * u[i+1];
+        #if USE_INFORMATION_ABOUT_A_STRUCTURE
+            if (i > 0){
+                if (updateMethod == JACOBI)
+                    u[i] += A[(getIndex(i, i-1, n))] * prevU[i-1];
+                else
+                    u[i] += A[(getIndex(i, i-1, n))] * u[i-1];
+            }
+            if (i < n - 1)
+                u[i] += A[getIndex(i, i + 1, n)] * u[i+1];
+        #else
+            for (int j = 0; j < n; ++j){
+                if (j == i) continue;
+                if (updateMethod == JACOBI)
+                    u[i] += (A[getIndex(i, j, n)] * prevU[j]);
+                else
+                    u[i] += (A[getIndex(i, j, n)] * u[j]);
+            }
+        #endif
         u[i] = (f[i] - u[i])/A[getIndex(i, i, n)];
     }
     if (updateMethod == JACOBI) free(prevU);
 }
+
 
 void solve(double *A, double *f, double *u, int n, method updateMethod){
     double initialResidual = computeResidual(A, f, u, n), currentResidual;
@@ -84,8 +149,9 @@ void solve(double *A, double *f, double *u, int n, method updateMethod){
         printf("iteration = %d, residual = %lf\n", iterCount, currentResidual);
     }
     if (iterCount != ITER_LIMIT)
-        printf("Residual decreased by a factor of 1000000 in %d iterations\n", iterCount);
+        printf("Residual decreased by a factor of 1,000,000 in %d iterations\n", iterCount);
 }
+
 
 method getUpdateMethod(int val){
     switch(val){
@@ -95,7 +161,9 @@ method getUpdateMethod(int val){
     return JACOBI;
 }
 
+
 int main(int argc, char *argv[]){
+    assert(argc == 3);
     int n = readInt(argv[1]);
     method updateMethod = getUpdateMethod(readInt(argv[2]));
     double *A, *f, *u;
@@ -108,6 +176,8 @@ int main(int argc, char *argv[]){
     clock_t end = clock();
     double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("Time taken: %f\n", cpu_time_used);
-    free(A); free(u); free(f);
+    free(A);
+    free(u);
+    free(f);
     return 0;
 }
--- assignment_1/problem3/run_execs.sh
+++ assignment_1/problem3/run_execs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-./laplaceO0 100 0 > output_jacobi.log
-./laplaceO3 100 1 > output_gs.log
-./laplaceO0 10000 0 > output_jacobi_10000.log
-./laplaceO3 10000 1 > output_gs_10000.log
+./laplaceO0 100 0 > output_jacobi_O0.log
+./laplaceO3 100 0 > output_jacobi_O3.log
+./laplaceO0 100 1 > output_gs_O0.log
+./laplaceO3 100 1 > output_gs_O3.log
+./laplaceO0 10000 0 > output_jacobi_10000_O0.log
+./laplaceO3 10000 0 > output_jacobi_10000_O3.log
+./laplaceO0 10000 1 > output_gs_10000_O0.log
+./laplaceO3 10000 1 > output_gs_10000_O3.log
