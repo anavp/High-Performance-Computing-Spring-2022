0b36586ef7273e75f6cc97697df9eca85c400921
anavp <anavprasadanav@gmail.com>
2022-04-18 05:26:40 -0400

    q1 code finally working; needs testing on cims; add comments

---
+++ assignment_4/matrix_vector.cu
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+#include <omp.h>
+
+#define BLOCK_SIZE 1024
+#define endl "\n"
+using namespace std;
+typedef long long int ll;
+
+inline ll getIndex(ll i, ll j, ll N){
+    return N * i + j;
+}
+
+void cpuMatrixVectorMult(double* ans, const double* mat, const double* vec, ll N, ll M) {
+    for (ll i = 0; i < M; i++){
+        double sum = 0;
+        #pragma omp parallel for schedule(static) reduction(+:sum)
+        for (ll j = 0; j < N; j++)
+            sum += mat[i * N + j] * vec[j];
+        ans[i] = sum;
+    }
+}
+
+__global__ void gpuMatrixVectorMult(double* intermediateMat, const double* mat, const double* vec, ll N, ll M) {
+    ll idx_x = blockIdx.x * blockDim.x + threadIdx.x;
+    ll idx_y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (idx_x < N && idx_y < M)
+        intermediateMat[idx_x * M + idx_y] = mat[idx_y * N + idx_x] * vec[idx_x];
+}
+
+
+__global__ void reduction_v2(double* sum, const double* a, ll N, ll M) {
+    __shared__ double sharedMemory[BLOCK_SIZE];
+    ll idx = (blockIdx.x) * blockDim.x + threadIdx.x;
+    ll idx_y = blockIdx.y;
+    if (idx < N) sharedMemory[threadIdx.x] = a[M * idx + idx_y];
+    else sharedMemory[threadIdx.x] = 0;
+
+    __syncthreads();
+    if (threadIdx.x < 512) sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 512];
+    __syncthreads();
+    if (threadIdx.x < 256) sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 256];
+    __syncthreads();
+    if (threadIdx.x < 128) sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 128];
+    __syncthreads();
+    if (threadIdx.x <  64) sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 64];
+    __syncthreads();
+    if (threadIdx.x <  32) {
+        sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 32];
+        __syncwarp();
+        sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 16];
+        __syncwarp();
+        sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 8];
+        __syncwarp();
+        sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 4];
+        __syncwarp();
+        sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 2];
+        __syncwarp();
+        if (threadIdx.x == 0) {
+            sum[M * blockIdx.x + idx_y] = sharedMemory[0] + sharedMemory[1];
+        }
+    }
+}
+
+int main() {
+    srand(rand());
+    cudaDeviceProp dev;
+    cudaGetDeviceProperties(&dev, 0);
+    cout << "Device Name: " << dev.name << endl;
+    cout << "Computation Capability: " << dev.major << "." << dev.minor << endl;
+    cout << "Memory: " << dev.totalGlobalMem * 1.0e-9 << endl;
+    cout << "Peak Memory Bandwidth (GB/s): " << 2.0 * dev.memoryClockRate * (dev.memoryBusWidth/8)/1.0e6;
+    cout << endl << endl;
+    
+    ll N = 1e5, M = 2e2, blockSize = 32;
+    ll gridDimsX = (N + blockSize - 1) / blockSize, gridDimsY = (M + blockSize - 1) / blockSize;
+    dim3 blockDims(blockSize, blockSize), gridDims(gridDimsX, gridDimsY);
+    
+    double *mat, *vec, *ans, *referenceAns;
+    referenceAns = (double* ) malloc(M*sizeof(double));
+    cudaMallocHost((void**)&mat, N * M * sizeof(double));
+    cudaMallocHost((void**)&vec, N * sizeof(double));
+    cudaMallocHost((void**)&ans, M * sizeof(double));
+
+    // Initialization:
+    for (ll i = 0; i < N; ++i){
+        vec[i] = ((double)rand())/((double)rand());
+        for (ll j = 0; j < M; ++j)
+            mat[getIndex(j, i, N)] = ((double)rand())/((double)rand());
+    }
+    for (ll i = 0; i < M; ++i){
+        ans[i] = 0;
+        referenceAns[i] = 0;
+    }
+
+    double time = omp_get_wtime();    
+    // CPU Computation
+    cpuMatrixVectorMult(referenceAns, mat, vec, N, M);
+    cout << "CPU Bandwidth = " << (M * N * sizeof(double)) / (omp_get_wtime() - time)/1e9 << " GB/s" << endl;
+    
+    double *deviceMat, *deviceVec, *deviceAns, *intermediateMat; 
+    cudaMalloc(&deviceMat, N * M * sizeof(double));
+    cudaMalloc(&intermediateMat, N * M * sizeof(double));
+    cudaMalloc(&deviceVec, N * sizeof(double));
+    time = omp_get_wtime();
+    cudaMemcpyAsync(deviceMat, mat, N * M * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpyAsync(deviceVec, vec, N * sizeof(double), cudaMemcpyHostToDevice);
+    cudaDeviceSynchronize();
+    gpuMatrixVectorMult <<<gridDims, blockDims>>> (intermediateMat, deviceMat, deviceVec, N, M);
+    cudaDeviceSynchronize();
+
+    ll N_work = M;
+    for (ll i = (N + BLOCK_SIZE - 1) / (BLOCK_SIZE); i > 1; i = (i + BLOCK_SIZE - 1) / (BLOCK_SIZE)) 
+        N_work += i*M;
+
+    double *tempVec;
+    cudaMalloc(&tempVec, N_work * sizeof(double));
+    deviceAns = tempVec;
+    ll N_rem = (N + BLOCK_SIZE - 1) / (BLOCK_SIZE);
+    dim3 gridDims2(N_rem, M), blockDims2(BLOCK_SIZE, 1);
+    reduction <<<gridDims2, blockDims2>>> (deviceAns, intermediateMat, N, M);
+    cudaDeviceSynchronize();
+    
+    while (N_rem > 1) {
+        ll N = N_rem;
+        N_rem = (N_rem + BLOCK_SIZE - 1) / (BLOCK_SIZE);
+        dim3 gridDims(N_rem, M);
+        reduction <<<gridDims, blockDims2>>> (deviceAns + N*M, deviceAns, N, M);
+        cudaDeviceSynchronize();
+        deviceAns += N * M;
+    }
+    cudaDeviceSynchronize();
+    
+    cudaMemcpyAsync(ans, deviceAns, M * sizeof(double), cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
+
+    cout << "GPU Bandwidth = " << N * M * sizeof(double) / (omp_get_wtime() - time) / 1e9 << " GB/s" << endl;
+    double error = 0.0;    
+    for (ll i = 0; i < M; ++i)
+        error += (ans[i] - referenceAns[i]) * (ans[i] - referenceAns[i]);
+    cout << fixed;
+    cout << setprecision(3) << "Error = " << error << endl;
+    
+    cudaFreeHost(mat);
+    cudaFreeHost(vec);
+    cudaFreeHost(ans);
+    free(referenceAns);
+    cudaFree(deviceMat);
+    cudaFree(deviceVec);
+    cudaFree(tempVec);
+    cudaFree(intermediateMat);
+    return 0; 
+}
---
+++ assignment_4/vector_vector.cu
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+#include <omp.h>
+
+#define BLOCK_SIZE 1024
+#define endl "\n"
+using namespace std;
+typedef long long int ll;
+
+
+double cpuProduct(const double* a, const double* b, ll N){
+    double sum = 0;
+    #pragma omp parallel for schedule(static) reduction(+:sum)
+    for (ll i = 0; i < N; i++) 
+        sum += a[i]*b[i];
+    return sum;
+}
+
+__global__ void reduction_product(double* sum, const double* a, const double* b = NULL, ll N = (1UL<<24), bool isReduction = true){
+    __shared__ double sharedMemory[BLOCK_SIZE];
+    int idx = (blockIdx.x) * blockDim.x + threadIdx.x;
+
+    if (idx < N) sharedMemory[threadIdx.x] = (isReduction ? a[idx]: a[idx]*b[idx]);
+    else sharedMemory[threadIdx.x] = 0;
+
+    __syncthreads();
+    if (threadIdx.x < 512) sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 512];
+    __syncthreads();
+    if (threadIdx.x < 256) sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 256];
+    __syncthreads();
+    if (threadIdx.x < 128) sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 128];
+    __syncthreads();
+    if (threadIdx.x < 64) sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 64];
+    __syncthreads();
+    if (threadIdx.x < 32) {
+        sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 32];
+        __syncwarp();
+        sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 16];
+        __syncwarp();
+        sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 8];
+        __syncwarp();
+        sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 4];
+        __syncwarp();
+        sharedMemory[threadIdx.x] += sharedMemory[threadIdx.x + 2];
+        __syncwarp();
+        if (threadIdx.x == 0) sum[blockIdx.x] = sharedMemory[0] + sharedMemory[1];
+    }
+}
+
+int main() {
+    srand(rand());
+    ll N = (1UL<<24);
+
+    double *vector1, *vector2;
+    cudaMallocHost((void**)&vector1, N * sizeof(double));
+    cudaMallocHost((void**)&vector2, N * sizeof(double));
+    
+    #pragma omp parallel for schedule(static)
+    for (ll i = 0; i < N; i++) {
+        vector1[i] = ((double)rand())/((double)rand());
+        vector2[i] = ((double)rand())/((double)rand());
+    }
+
+    double referenceSum, sum;
+    double time = omp_get_wtime();
+    referenceSum = cpuProduct(vector1, vector2, N);
+    cout << "CPU Bandwidth = " << N * sizeof(double) / (omp_get_wtime() - time) / 1e9 << " GB/s" << endl;
+
+    double *deviceVector1, *deviceVector2, *intermediateVector;
+    cudaMalloc(&deviceVector1, N*sizeof(double));
+    cudaMalloc(&deviceVector2, N*sizeof(double));
+    ll N_work = 1;
+    for (ll i = (N+BLOCK_SIZE-1)/(BLOCK_SIZE); i > 1; i = (i+BLOCK_SIZE-1)/(BLOCK_SIZE)) 
+        N_work += i;
+    cudaMalloc(&intermediateVector, N_work * sizeof(double)); // extra memory buffer for reduction across thread-blocks
+
+    cudaMemcpyAsync(deviceVector1, vector1, N * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpyAsync(deviceVector2, vector2, N * sizeof(double), cudaMemcpyHostToDevice);
+    cudaDeviceSynchronize();
+    time = omp_get_wtime();
+
+    double* sum_d = intermediateVector;
+    ll N_rem = (N+BLOCK_SIZE-1)/(BLOCK_SIZE);
+    reduction_product<<<N_rem,BLOCK_SIZE>>>(sum_d, deviceVector1, deviceVector2, N, false);
+    
+    while (N_rem > 1) {
+        ll N = N_rem;
+        N_rem = (N_rem+BLOCK_SIZE-1)/(BLOCK_SIZE);
+        reduction_product<<<N_rem,BLOCK_SIZE>>>(sum_d + N, sum_d, NULL, N);
+        sum_d += N;
+    }
+
+    cudaMemcpyAsync(&sum, sum_d, sizeof(double), cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
+    cout << "GPU Bandwidth = " << N * sizeof(double) / (omp_get_wtime() - time) / 1e9 << endl;
+    cout << fixed;
+    cout << setprecision(4) << "Error = " << abs(sum - referenceSum) << endl;
+
+    cudaFree(deviceVector1);
+    cudaFree(deviceVector2);
+    cudaFreeHost(vector1);
+    cudaFreeHost(vector2);
+
+    return 0;
+}
